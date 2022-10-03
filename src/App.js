8f877d1a5aa30e6fec34a0a59a495befc9b3ab6f
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-03 14:04:26 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import SisRoutes from "./SisRoutes";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Principal from "./Components/Principal";
 
 function App() {
   return (
